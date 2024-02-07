04107b707d6dff7e254e5c4993755e5670d48cca
aurielprepejchal <aurielprepejchal@localhost>
2024-02-07 17:17:03 -0600

    nice job guys

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,17 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Type in a number for n higher than 1!");
+		int n = in.nextInt();
+		int[]nsieve = new int[n+1];
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
